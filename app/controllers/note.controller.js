dec58e59e2881eaedb3ea9c9c10a7defb230c35b
Nick Playfair <nick@chaosemerald.co.uk>
2018-08-23 14:49:38 +0100

    outline controller functions

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,26 @@
+const Note = require('../models/note.model.js');
+
+// create and save new note
+exports.create = (req, res) => {
+
+};
+
+// retrieve and return all notes from the database
+exports.findAll = (req, res) => {
+
+};
+
+// find a single note with noteId
+exports.findOne = (req, res) => {
+
+};
+
+// update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+};
+
+// delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+};
